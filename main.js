4228d158cbbc3068eb0a26628153b0e5e4963509
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2022-11-29 10:57:48 +0200

    implementing text editor

---
+++ main.js
@@ -0,0 +1,8 @@
+
+tinymce.init({
+    selector: "textarea.simple",
+    plugins: "advlist lists spellchecker autoresize wordcount",
+    min_height: 120,
+    menubar: false,
+    toolbar: "bold italic underline | bullist numlist | spellchecker"
+  });
